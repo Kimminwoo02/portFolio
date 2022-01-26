a1d21d3a419fb5d8529a9b9939e7c8ba8930c585
minu <made_power@naver.com>
2022-01-26 15:56:17 +0900

    tab menu add

--- script/script.js
+++ script/script.js
@@ -9,9 +9,11 @@ $(function() {
         captions: true,
         autoHover: false,
         controls: true,
-        nextText: 'next',
-        prevText: 'prev',
-        controls: false,
+
+        infiniteLoop: true,
+        auto: true,
+
+
 
     });
 });
@@ -25,4 +27,54 @@ $(document).ready(function() {
         autoScrolling: true,
         scrollHorizontally: true
     });
+});
+
+
+// 탭 메뉴
+$(document).ready(function() {
+
+
+    //----------Select the first tab and div by default
+
+    $('#vertical_tab_nav > ul > li > a').eq(0).addClass("selected");
+    $('#vertical_tab_nav > div > article').eq(0).css('display', 'block');
+
+
+    //---------- This assigns an onclick event to each tab link("a" tag) and passes a parameter to the showHideTab() function
+
+    $('#vertical_tab_nav > ul').click(function(e) {
+
+        if ($(e.target).is("a")) {
+
+            /*Handle Tab Nav*/
+            $('#vertical_tab_nav > ul > li > a').removeClass("selected");
+            $(e.target).addClass("selected");
+
+            /*Handles Tab Content*/
+            var clicked_index = $("a", this).index(e.target);
+            $('#vertical_tab_nav > div > article').css('display', 'none');
+            $('#vertical_tab_nav > div > article').eq(clicked_index).fadeIn();
+
+        }
+
+        $(this).blur();
+        return false;
+
+    });
+
+
+}); //end ready
+
+/* if in drawer mode */
+$(".tab_drawer_heading").click(function() {
+
+    $("article").hide();
+    var d_activeTab = $(this).attr("rel");
+    $("#" + d_activeTab).fadeIn();
+
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(this).addClass("d_active");
+
+    $("ul.tabs li a").removeClass("selected");
+    $("ul.tabs li a[rel^='" + d_activeTab + "']").addClass("selected");
 });
